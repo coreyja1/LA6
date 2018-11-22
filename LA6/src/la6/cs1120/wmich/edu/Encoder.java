010d81d3ee9aa996d0c326cfad89a3fd887bc314
Jacob's PC <Jacob's PC@DESKTOP-GV6NPBD.attlocal.net>
2018-11-21 23:23:21 -0500

    Jacob commit 2

---
+++ LA6/src/la6/cs1120/wmich/edu/Encoder.java
@@ -0,0 +1,66 @@
+package la6.cs1120.wmich.edu;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Encoder implements IEncoder {
+
+	@Override
+	/**
+	 * This method takes the inputFileName and reads the file. It will write each
+	 * character followed by an integer between 1 and 20 to a binary file specified
+	 * by outputFilePath after each random integer it will move x spaces in the
+	 * file, where x is the random integer for each character.
+	 */
+	public void encode(String inputFileName, String outputFilePath) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		int location = 0;
+		int x = 0;
+		try {
+			File file = new File(inputFileName);
+			Scanner sc = new Scanner(file);
+			RandomAccessFile output = new RandomAccessFile(outputFilePath, "rw");
+			String line = sc.nextLine();
+			char[] letters = line.toCharArray();
+
+			for (int i = 0; i < letters.length; i++) {
+
+				// generates a random integer between 1 and 20
+				int randomInt = rand.nextInt(20) + 1;
+				output.writeChar(letters[i]);
+
+				// if it is the last character in the file add -1 after the character
+				if (i == (letters.length - 1)) {
+					output.writeInt(-1);
+				} else {
+					output.writeInt(randomInt);
+					for (int j = 0; j < randomInt; j++) {
+
+						byte[] randByte = new byte[randomInt];
+						new Random().nextBytes(randByte);
+						output.writeByte(randByte[j]);
+					}
+					if (i == 0) {
+						x = 6 + (randomInt);
+						location = x;
+					} else {
+						location = (x + 6 + randomInt);
+						x = location;
+
+					}
+					output.seek(location);
+					
+				}
+			}
+			sc.close();
+			output.close();
+		} catch (IOException e) {
+			System.out.println("The file was not found.");
+		}
+	}
+
+}
---
+++ LA6/src/la6/cs1120/wmich/edu/IDecoder.java
@@ -0,0 +1,8 @@
+package la6.cs1120.wmich.edu;
+
+public interface IDecoder {
+	// Given the file path of the binary file, the decoder will read the file, decode the message and 
+		// print it to the console.
+		// Include code to handle the IOException.
+		public void decode(String filePath);
+}
